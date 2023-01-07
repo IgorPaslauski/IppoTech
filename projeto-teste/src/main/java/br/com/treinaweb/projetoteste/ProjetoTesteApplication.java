07621316e49dd29621c50cb13245486d9b692561
Igor Paslauski Pedroso de Oliveira <io006235@cfjl.com.br>
2023-01-07 15:42:51 -0300

    Creacao do spring boot

---
+++ projeto-teste/src/main/java/br/com/treinaweb/projetoteste/ProjetoTesteApplication.java
@@ -0,0 +1,13 @@
+package br.com.treinaweb.projetoteste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoTesteApplication.class, args);
+	}
+
+}
